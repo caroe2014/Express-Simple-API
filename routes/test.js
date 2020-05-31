b6671d3bb332c54ee89ef054f90b6c155c98aa2d
caroe2014 <edwincaro10@hotmail.com>
2020-05-30 20:03:48 -0500

    more changes to routes

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json([{test: 'Hello', job: 'This'}, {user: 'is', job: 'a Success'}, {user: 'Test', job: 'congratulation'}]); 
+  res.json([{hello: 'Hello World. ', job: 'This'}, {user: 'is', job: 'a Success'}, {user: 'Test', job: 'congratulation'}]); 
 });
-
+router.get('/', function(req, res, next) {
+  res.json([{test: 'Test', job: 'This'}, {user: 'a', job: 'a Success'}, {user: 'Test', job: 'congratulation'}]); 
+});
 module.exports = router;
