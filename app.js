98b55b32c911bfc6479ecde3c791283b6fca1603
caroe2014 <edwincaro10@hotmail.com>
2020-05-30 20:29:26 -0500

    OMG more changes in index.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testRouter = require('./routes/test');
 
+
 var app = express();
 
 app.use(function(req, res, next) { 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test', testRouter);
+app.use('/hello', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
